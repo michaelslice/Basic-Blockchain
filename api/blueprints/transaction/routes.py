2f8c4669dfc6c619b2ff88aa0b803d4ce9e71541
michaelslice <mjsens523@gmail.com>
2024-08-30 01:08:45 -0500

    Working on transaction

--- api/blueprints/transaction/routes.py
+++ api/blueprints/transaction/routes.py
@@ -1,13 +1,29 @@
 # Routes related to transaction
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from . import transaction_bp
 import hashlib
 import random
+import json 
 
 @transaction_bp.route('/transaction', methods=['POST'])
 def transaction():
-    if request.method == "POST":
-        print("TEST")
-    else:
-        return {"Error": "Invalid Request"}, 400     
+    
+    data = request.get_json()
+    
+    # User1 wallet info
+    User1PrivateKey = data.get("User1-Private-Key")
+    User1PublicKey = data.get("User1-Public-Key")
+    User1Bitcoin = data.get("User1-Bitcoin")
+    
+    # User2 wallet info
+    User2PrivateKey = data.get("User2-Private-Key")
+    User2PublicKey = data.get("User2-Public-Key")
+    User2Bitcoin = data.get("User2-Bitcoin")
+    
+    if 
+    
+    
+    
+    
+    return jsonify(data)     
     
--- script.sh
+++ script.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-url="http://100.110.149.130:5000/wallet"
+# Flask API for generating public and private keys
+url="http://127.0.0.1:5000/wallet"
 
+# Get User 1's Public and Private Keys
 response=$(curl "$url")
-echo "$response"
-
-
 user1_public_key=$(echo "$response" | jq -r '.private_key')  
 user1_private_key=$(echo "$response" | jq -r '.public_key | join(",")')
+echo "User 1 Private Key $user1_private_key"
+echo "User 1 Public Key $user1_public_key"
 
-# user2_public_key = 
-# user2_private_key = 
+# Get User 2's Public and Private Keys
+response=$(curl "$url")
+user2_public_key=$(echo "$response" | jq -r '.private_key')  
+user2_private_key=$(echo "$response" | jq -r '.public_key | join(",")')
+echo "User 2 Private Key $user2_private_key"
+echo "User 2 Public Key $user2_public_key"
+
+# Conduct a transaction between them
+transaction_url="http://127.0.0.1:5000/transaction"
+
+curl -X POST $transaction_url \
+    -H "Content-Type: application/json" \
+    -d "{
+            \"User1-Public-Key\": \"$user1_public_key\", 
+            \"User1-Private-Key\": \"$user1_private_key\", 
+            \"User2-Public-Key\": \"$user2_public_key\", 
+            \"User2-Private-Key\": \"$user2_private_key\",
+
+            \"User1-Bitcoin\": \"3\",
+            \"User2-Bitcoin\": \"2\"
+        }"
+    
